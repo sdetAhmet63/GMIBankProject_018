62c127fed80f12faa97fd62f4c1b81825195b50f
hamdiyazici <yazicihamdi@gmail.com>
2020-12-10 11:57:11 -0500

    Hamdi 3rd commint

--- src/test/java/gmibank/runners/Runner.java
+++ src/test/java/gmibank/runners/Runner.java
@@ -5,14 +5,14 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
+@CucumberOptions( strict = true,
         plugin = {"html:target/default-cucumber-reports",
                 "json:target/json-reports/cucumber.json",
                 "junit:target/xml-report/cucumber.xml"
         },
         features = "src/test/resources/features",
         glue = "gmibank/stepdefinitions",
-        tags = "",
+        tags = "@US01",
         dryRun = false
 )
 public class Runner {
