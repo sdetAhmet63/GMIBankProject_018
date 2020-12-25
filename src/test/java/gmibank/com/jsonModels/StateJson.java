cda5674def89c122c9b8e68d627772a3d151db93
mk2019dnz <74745591+mk2019dnz@users.noreply.github.com>
2020-12-25 20:56:44 +0300

    "25.12" (#40)
    * "25.12"
    
    * 25.12 son
    
    * son

--- src/test/java/gmibank/com/jsonModels/StateJson.java
+++ src/test/java/gmibank/com/jsonModels/StateJson.java
@@ -2,6 +2,9 @@ package gmibank.com.jsonModels;
 
 public class StateJson {
 
-    //final public static  String CREATE_STATE = "{\"countryId\": 1,\"name\":\"World2\" }";
-     final public static  String CREATE_STATE2 = "{\"countryId\": 1,\"name\":\"Belgium\" }";
+
+    final public static  String CREATE_STATE = "{\"countryId\": 1,\"name\":\"World2\" }";
+   // final public static  String CREATE_STATE = "{\"countryId\": 1,\"name\":\"World2\" }";
+
 }
+
